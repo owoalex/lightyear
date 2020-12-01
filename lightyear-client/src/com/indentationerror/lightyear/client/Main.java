e17430435643a7c6f10552dd3625602251ff9640
Alex <alexbaldwin2876@outlook.com>
2020-12-01 22:23:07 +0000

    Starting C Port

--- lightyear-client/src/com/indentationerror/lightyear/client/Main.java
+++
@@ -1,9 +0,0 @@
-package com.indentationerror.lightyear.client;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-        GUI gui = new GUI();
-    }
-}
